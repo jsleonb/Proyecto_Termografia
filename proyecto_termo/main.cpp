0dad089a59f3d6a4928528ab8c5ba8f42bcc4ea7
Jorge <Jorge@Jorges-iMAC.local>
2015-06-27 13:18:25 -0300

    Initial Commit

---
+++ proyecto_termo/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  proyecto_termo
+//
+//  Created by Jorge on 27-06-15.
+//  Copyright (c) 2015 Jorge. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
